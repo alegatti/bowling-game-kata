677e628cd403f3b3a1c193a295e592901b4aef6c
Alessandro Gatti <ale.gatti96@gmail.com>
2017-06-27 01:09:04 +0200

    Add get_games.php and save_game.php

--- backend/get_games.php
+++ backend/get_games.php
@@ -1,4 +1,21 @@
 <?php
+    //error_reporting(E_ERROR);
+    //ini_set('display_errors', 1);
 
+    require_once '../config.php';
+    
+    $mysqli = new mysqli($servername, $username, $password, $dbname);
+    $response = array("status" => false);
+    if ($result = $mysqli->query("SELECT * FROM games")) {
+        while($row = $result->fetch_array(MYSQL_ASSOC)) {
+                $json[] = $row;
+        }
+        $response['data'] = $json;
+        $response['status'] = true;
+    }
 
+    $result->close();
+    $mysqli->close();
+
+    echo json_encode($response);
 ?>
--- backend/save_game.php
+++ backend/save_game.php
@@ -1,4 +1,45 @@
 <?php
+    //error_reporting(E_ERROR);
+    //ini_set('display_errors', 1);
 
+    $response = array("status" => false);
 
+    try {
+        include '../config.php';
+
+        if(!isset($_POST))
+            throw new Exception('Invalid data');
+        
+        if(!isset($_POST['player_name']))
+            throw new Exception('Invalid player name');
+        $player_name = $_POST['player_name'];
+
+        if(!isset($_POST['frames']))
+            throw new Exception('Invalid frames');
+        $frames = $_POST['frames'];
+
+        $mysqli = new mysqli($servername, $username, $password, $dbname);
+        if ($mysqli->connect_error)
+            throw new Exception("Connection failed: " . $mysqli->connect_error);
+
+        $stmt = $mysqli->prepare("INSERT INTO games (player_name, frames, play_date) VALUES (?, ?, NOW())");
+        if(!$stmt)
+            throw new Exception("Invalid prepared statement");
+        $stmt->bind_param("ss", $player_name, $frames);
+        $stmt->execute();
+ 
+        if($mysqli->error)
+            throw new Exception($mysqli->error);
+        
+        $stmt->close();
+        $mysqli->close();
+
+        $response['status'] = true;
+
+    } catch(Exception $err) {
+        $response['err'] = $err->getMessage();
+    }
+
+    echo json_encode($response);
+    
 ?>
